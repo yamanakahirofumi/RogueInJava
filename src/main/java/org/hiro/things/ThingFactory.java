a238dc90c8a30835be104dfde31135f782e393ff
hiro <mokojarasi@gmail.com>
2019-01-19 09:56:20 +0900

    名称変更

--- src/main/java/org/hiro/things/ThingFactory.java
+++ src/main/java/org/hiro/things/ThingFactory.java
@@ -35,40 +35,44 @@ public class ThingFactory {
             case 5:
                 return new Ring();
             case 6:
-                StickEnum s = StickEnum.get(ThingMethod.pick_one(Arrays.asList(Global.ws_info), StickEnum.getMaxValue()));
-                switch (s) {
-                    case LightUp:
-                        return new LightUp();
-                    case InvisibleSeeing:
-                        return new InvisibleSeeing();
-                    case ElectricBolt:
-                        return new ElectricBolt();
-                    case FireBolt:
-                        return new FireBolt();
-                    case IceBolt:
-                        return new IceBolt();
-                    case ChangeMoster:
-                        return new ChangeMonster();
-                    case MagicMissile:
-                        return new MagicMissile();
-                    case HasteMonster:
-                        return new HasteMonster();
-                    case SlowMonster:
-                        return new SlowMonster();
-                    case Drain:
-                        return new Drain();
-                    case TeleportAway:
-                        return new TeleportAway();
-                    case TeleportMe:
-                        return new TeleportMe();
-                    case InvalidateEffect:
-                        return new InvalidateEffect();
-                    case NoEffect:
-                    default:
-                        return new Stick();
-                }
+                return createStick();
             default:
                 throw new RuntimeException("Bugs!!");
+        }
+    }
+
+    private static Stick createStick(){
+        StickEnum s = StickEnum.get(ThingMethod.pick_one(Arrays.asList(Global.ws_info), StickEnum.getMaxValue()));
+        switch (s) {
+            case LightUp:
+                return new LightUp();
+            case InvisibleSeeing:
+                return new InvisibleSeeing();
+            case ElectricBolt:
+                return new ElectricBolt();
+            case FireBolt:
+                return new FireBolt();
+            case IceBolt:
+                return new IceBolt();
+            case ChangeMoster:
+                return new ChangeMonster();
+            case MagicMissile:
+                return new MagicMissile();
+            case HasteMonster:
+                return new HasteMonster();
+            case SlowMonster:
+                return new SlowMonster();
+            case Drain:
+                return new Drain();
+            case TeleportAway:
+                return new TeleportAway();
+            case TeleportMe:
+                return new TeleportMe();
+            case InvalidateEffect:
+                return new InvalidateEffect();
+            case NoEffect:
+            default:
+                return new Stick();
         }
     }
 }
